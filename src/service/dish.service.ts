92f97eb231937005fb8f15c0e94efe6df046a8ca
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-05-28 14:43:03 +0700

    feat: Add API endpoint to get daily dishes

--- src/service/dish.service.ts
+++ src/service/dish.service.ts
@@ -1,7 +1,29 @@
+import { Dish } from "@prisma/client";
 import { prisma } from "../prisma.js";
+import { getRedisAsync, setRedisAsync } from "../redis.js";
 
 export default class DishService {
     static async getDishes() {
         return await prisma.dish.findMany();
+    }
+    static async getDishDaily() {
+        const today = new Date().toISOString().split('T')[0];
+        const cacheKey = `daily-dishes-${today}`;
+        let dailyDishes = await getRedisAsync(cacheKey);
+        if (dailyDishes) {
+            return JSON.parse(dailyDishes);
+        }
+        const allDish = await prisma.dish.findMany();
+        const dishesTypes = [...new Set(allDish.map(dish => dish.type))];
+        const selectedDishes: { [key: string]: Dish } = {};
+        for (const type of dishesTypes) {
+            const dishes = allDish.filter(dish => dish.type === type);
+            if (dishes.length > 0) {
+                const randomIndex = Math.floor(Math.random() * dishes.length);
+                selectedDishes[type] = dishes[randomIndex];
+            }
+        }
+        await setRedisAsync(cacheKey, JSON.stringify(selectedDishes), 60);
+        return selectedDishes
     }
 }
