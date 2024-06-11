ba83f851f049516365b3db51a7402b7bf57a7458
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-06-11 23:48:17 +0700

    chore: Add express.json middleware for parsing JSON data in requests

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ const app = express()
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 app.use(cors())
+app.use(express.json())
 app.use("/", await router({
     directory: path.join(__dirname, "routes"),
 }))
